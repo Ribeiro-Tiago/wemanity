6e32f9fb25f743adc76400dd9912bc9a7e1f944a
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2019-05-25 20:42:32 +0100

    added forms angular forms to home module updated search bar input directive

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { HomeComponent } from "./home.component";
 import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
@@ -11,6 +12,7 @@ import { SearchBarComponent } from "../../components/search-bar/search-bar.compo
     ],
     imports: [
         BrowserModule,
+        FormsModule,
     ],
     providers: [],
     bootstrap: [HomeComponent]
